62ed3dc0162e9315ae3665327202096c06957b45
Cogitium <info@cogitium.com>
2022-04-15 14:03:51 +0200

    Maj form post

--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -3,23 +3,31 @@
 namespace App\Form;
 
 use App\Entity\Post;
+use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class PostType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('slug')
-            ->add('content')
-            ->add('image')
-            ->add('createdAt')
-            ->add('active')
-            ->add('user')
-            ->add('category')
+            ->add('title', TextType::class)
+            //->add('slug')
+            ->add('content', TextareaType::class)
+            ->add('image', TextType::class)
+            //->add('createdAt')
+            //->add('active')
+            //->add('user')
+            ->add('category', EntityType::class, [
+                'class' => Category::class
+            ])
+            ->add('Valider', SubmitType::class)
         ;
     }
 
