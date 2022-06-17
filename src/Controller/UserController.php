3495c730df89e96384dbba6d69246c81d3f060bd
Cogitium <info@cogitium.com>
2022-06-17 18:48:27 +0200

    Dev de la modification de mot de passe

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Form\PassType;
 use App\Form\UserType;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Request;
@@ -9,6 +10,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserController extends AbstractController
 {
@@ -31,6 +33,49 @@ class UserController extends AbstractController
         }
 
         return $this->render('user/profil.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[IsGranted('ROLE_USER')]
+    #[Route('/password', name: 'app_password')]
+    public function password(Request $request, UserPasswordHasherInterface $userPasswordHasher, ManagerRegistry $doctrine): Response
+    {
+        $user = $this->getUser();
+        
+        $form = $this->createForm(PassType::class, $user);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            // On vérifie que l'ancien mot de passe est correct
+            $oldPassword = $form->get('oldPassword')->getData();
+            if (!$userPasswordHasher->isPasswordValid($user, $oldPassword)) {
+
+                // On redirige sur le même form avec un feedback
+                $this->addFlash('danger', 'Mot de passe actuel erroné !');
+                return $this->redirectToRoute('app_password');
+            }
+
+            // On hash le nouveau mot de passe
+            $encodedPassword = $userPasswordHasher->hashPassword(
+                $user,
+                $form->get('plainPassword')->getData()
+            );
+
+            // On le stocke dans l'instance user
+            $user->setPassword($encodedPassword);
+
+            // On update le user dans la BD
+            $em = $doctrine->getManager();
+            $em->flush();
+
+            // On redirige vers homme avec feedback
+            $this->addFlash('success', 'Mot de passe modifié !');
+            return $this->redirectToRoute('home');
+        }
+
+        return $this->render('user/password.html.twig', [
             'form' => $form->createView(),
         ]);
     }
---
+++ src/Form/PassType.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+
+class PassType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('oldPassword', PasswordType::class, [
+                'mapped' => false,
+                'label' => 'Mot de passe actuel',
+                'attr' => ['autocomplete' => 'email'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Mot de passe actuel',
+                    ]),
+                ],
+            ])
+            ->add('plainPassword', RepeatedType::class, [
+                'mapped' => false,
+                'type' => PasswordType::class,
+                'first_options' => [
+                    'attr' => ['autocomplete' => 'new-password'],
+                    'label' => 'Nouveau mot de passe',
+                    'constraints' => [
+                        new NotBlank([
+                            'message' => 'Entrez un mot de passe',
+                        ]),
+                        new Length([
+                            'min' => 6,
+                            'minMessage' => 'Votre mot de passe doit contenir au moins {{ limit }} caractères',
+                            // max length allowed by Symfony for security reasons
+                            'max' => 4096,
+                        ]),
+                    ],
+                ],
+                'second_options' => [
+                    'attr' => ['autocomplete' => 'new-password'],
+                    'label' => 'Répétez le mot de passe',
+                ],
+                'invalid_message' => 'Les mots de passe ne correspondent pas !',
+                // Instead of being set onto the object directly,
+                // this is read and encoded in the controller
+            ])
+            ->add('Valider', SubmitType::class, [
+                    "label" => "Modifier",
+                ])
+            ;
+        
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
