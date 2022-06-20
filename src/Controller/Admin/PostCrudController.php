da5cde0af0f9965ba82eca89f8acfe56e8b851e7
Cogitium <info@cogitium.com>
2022-06-20 17:43:20 +0200

    Maj BO

--- src/Controller/Admin/PostCrudController.php
+++ src/Controller/Admin/PostCrudController.php
@@ -9,6 +9,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
@@ -53,6 +54,7 @@ class PostCrudController extends AbstractCrudController
             TextField::new('title'),
             AssociationField::new('category'),
             TextEditorField::new('content')->onlyOnForms()->setFormType(CKEditorType::class),
+            IntegerField::new('views'),
             AssociationField::new('tags'),
             // NumberField::new('price')->setNumDecimals(2),
             MoneyField::new('price')->setStoredAsCents(false)->setCurrency('EUR'),
